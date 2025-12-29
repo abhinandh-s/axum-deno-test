a16c2d388e7fa618b17e71d709f28894668d4627
Abhinandh S <abhinandhsuby@proton.me>
2025-12-29 14:44:09 +0530

    fix!: now works with deno deploy

--- flake.nix
+++ flake.nix
@@ -1,46 +1,38 @@
+
 {
-  description = "A basic Rust devshell for NixOS users developing Leptos";
+  description = "Rust + wasm32 devshell using oxalica/rust-overlay";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      nixpkgs,
-      rust-overlay,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+        pkgs = import nixpkgs { inherit system overlays; };
       in
-      with pkgs;
       {
-        devShells.default = mkShell {
-          buildInputs =
-            [
-              (rust-bin.selectLatestNightlyWith (
-                toolchain:
-                toolchain.default.override {
-                  extensions = [
-                    "rust-src"
-                    "rust-analyzer"
-                  ];
-                  targets = [ "wasm32-unknown-unknown" ];
-                }
-              ))
-            ];
+        devShells.default = pkgs.mkShell {
+          name = "rust-wasm-devshell";
 
-          shellHook = '''';
+          buildInputs = [
+            # Rust nightly pinned using selectLatestNightlyWith (reliable)
+            (pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+              extensions = [ "rust-src" "rust-analyzer" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            }))
+          ];
+
+          shellHook = ''
+            echo "Welcome to Rust + wasm32 devshell"
+            rustc --version
+            cargo --version
+          '';
         };
       }
     );
 }
+
