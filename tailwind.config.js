dca3c00d691bd4c8eede2b68e0c2393ec0bb54dd
Abhinandh S <abhinandhsuby@proton.me>
2026-01-16 11:47:59 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.html"], // Tailwind will scan your Askama templates
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
